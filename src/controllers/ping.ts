44148053ff2371bbfdb182bbc3cecb7b4ab65169
Isaac Adewumi <ienioladewumi@gmail.com>
2023-07-13 19:19:32 +0100

    test(*): add test for ping endpoint

--- src/controllers/ping.ts
+++ src/controllers/ping.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
-const index = (req: Request, res: Response): Response => {
-	return res.status(httpStatus.OK);
+const index = (req: Request, res: Response): void => {
+	res.status(httpStatus.OK).json();
 };
 
 export default index;
