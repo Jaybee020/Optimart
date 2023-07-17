1f83b9e04ab39bdfc5d8e70d2f1d565dbd1a5258
Isaac Adewumi <ienioladewumi@gmail.com>
2023-07-17 18:43:32 +0100

    feat(*): ran db migrations and updated config

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 const configSchema = z.object({
 	PORT: z.number().default(3000),
 	SECRET_KEY: z.string().nonempty(),
+	DATABASE_URL: z.string().url().nonempty(),
 	NODE_ENV: z.string().default('development'),
 	XRPL_NODE_URL: z.string().url().nonempty(),
 });
