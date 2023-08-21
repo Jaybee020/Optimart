6b26f98687cfe0820a3a7c06c378687e8d94b60c
Isaac Adewumi <ienioladewumi@gmail.com>
2023-08-21 08:54:19 +0100

    cleanup

--- listings/tasks.py
+++ listings/tasks.py
@@ -7,7 +7,6 @@ from django.conf import settings
 from django.utils import timezone
 
 from listings.enums import ListingStatus, ListingType
-from listings.mixins import OfferActionsMixin
 from listings.models import Listing, Offer
 from services.resend import ResendMail
 
@@ -19,6 +18,8 @@ resend_client = ResendMail(
 
 @db_periodic_task(crontab(minute=1))
 def monitor_auctions():
+    from listings.mixins import OfferActionsMixin
+
     logger.info('Monitoring auctions...')
     ongoing_auctions = Listing.objects.filter(
         status=ListingStatus.ONGOING,
@@ -45,46 +46,47 @@ def monitor_auctions():
 @db_task()
 def notify_offer_accepted_and_sold(offer_id: int):
     offer = Offer.objects.get(id=offer_id)
+    if offer.creator.email is not None:
+        creator_subject = '[Optimart] Your Offer Was Accepted!'
+        creator_content = f"""Congratulations!
 
-    # Notify offer creator
-    creator_subject = '[Optimart] Your Offer Was Accepted!'
-    creator_content = f"""Congratulations!
+        Your offer for the item "{offer.nft.name}" with ID {offer.nft.token_identifier} has been accepted.
+        The transaction hash of the sale is {offer.update_tx_hash}
 
-    Your offer for the item "{offer.nft.name}" with ID {offer.nft.token_identifier} has been accepted.
-    The transaction hash of the sale is {offer.update_tx_hash}
+        Love,
+        Team Optimart.
+        """
 
-    Love,
-    Team Optimart.
-    """
+        resend_client.send_email(
+            to=offer.creator.email,
+            subject=creator_subject,
+            content=creator_content,
+        )
 
-    resend_client.send_email(
-        to=offer.creator.email,
-        subject=creator_subject,
-        content=creator_content,
-    )
+    if offer.nft.owner.email is not None:
+        owner_subject = '[Optimart] Your Item Was Sold!'
+        owner_content = f"""
+        Great news!
 
-    # Notify NFT owner
-    owner_subject = '[Optimart] Your Item Was Sold!'
-    owner_content = f"""
-    Great news!
+        Your item "{offer.nft.name}" with ID {offer.nft.token_identifier} has been sold to a buyer.
+        The transaction hash of the sale is {offer.update_tx_hash}
 
-    Your item "{offer.nft.name}" with ID {offer.nft.token_identifier} has been sold to a buyer.
-    The transaction hash of the sale is {offer.update_tx_hash}
+        Love,
+        Team Optimart.
+        """
 
-    Love,
-    Team Optimart.
-    """
-
-    resend_client.send_email(
-        to=offer.nft.owner.email,
-        subject=owner_subject,
-        content=owner_content,
-    )
+        resend_client.send_email(
+            to=offer.nft.owner.email,
+            subject=owner_subject,
+            content=owner_content,
+        )
 
 
 @db_task()
 def notify_offer_received(offer_id: int):
     offer = Offer.objects.get(id=offer_id)
+    if offer.nft.owner.email is None:
+        return
 
     owner_subject = '[Optimart] New Offer Received'
     owner_content = f"""
