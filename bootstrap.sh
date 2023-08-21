c2e0b3e4d10dd9580870db12854e9eb9de44af41
Isaac Adewumi <ienioladewumi@gmail.com>
2023-08-21 09:23:02 +0100

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Starting Gunicorn server..."
-gunicorn optimart.wsgi --capture-output --log-level info --workers 3  --daemon
+gunicorn optimart.wsgi --capture-output --log-level info --workers 4 --enable-stdio-inheritance  --daemon
 
 echo "Starting Huey task queue..."
 python manage.py run_huey
