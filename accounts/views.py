8151a86d7ca7877704dce2e405f01f25c98a2939
Isaac Adewumi <ienioladewumi@gmail.com>
2023-08-21 13:11:27 +0100

    s

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.generics import CreateAPIView, GenericAPIView, RetrieveUpdateAPIView
 
 from accounts.models import Account
@@ -18,3 +19,6 @@ class AccountAPIView(RetrieveUpdateAPIView):
     queryset = Account
     lookup_field = 'address'
     serializer_class = AccountSerializer
+
+    def put(self, request, *args, **kwargs):  # noqa: ARG002
+        raise MethodNotAllowed
